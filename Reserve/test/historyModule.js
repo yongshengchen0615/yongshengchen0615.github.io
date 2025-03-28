caad29ff5d0c970040b2f5e0e0e1239e2029b7aa
yongshengchen0615 <yongshengchen.tw@gmail.com>
2025-03-28 17:01:24 +0800

    v59

---
+++ Reserve/test/historyModule.js
@@ -0,0 +1,45 @@
+// historyModule.js
+export const HistoryModule = (() => {
+    function saveLastBooking(data) {
+        localStorage.setItem("lastBookingData", JSON.stringify(data));
+    }
+
+    function restoreLastBooking() {
+        const lastBooking = JSON.parse(localStorage.getItem("lastBookingData"));
+        if (!lastBooking) return;
+
+        $("#name").val(lastBooking.name);
+        $("#phone").val(lastBooking.phone);
+        $("#booking-type").val(lastBooking.bookingTypeText === "代訂他人" ? "other" : "self");
+        $("#booking-date").val(lastBooking.date);
+        $("#booking-time").val(lastBooking.time);
+        $("#num-people").val(lastBooking.numPeople).trigger("change");
+
+        setTimeout(() => {
+            lastBooking.persons.forEach((person, index) => {
+                const card = $(`.person-card[data-person="${index}"]`);
+
+                person.mainServices.forEach(service => {
+                    const select = card.find(".main-service");
+                    if (select.find(`option[value="${service}"]`).length) {
+                        select.val(service);
+                        card.find(".add-service[data-type='main']").click();
+                    }
+                });
+
+                person.addonServices.forEach(service => {
+                    const select = card.find(".addon-service");
+                    if (select.find(`option[value="${service}"]`).length) {
+                        select.val(service);
+                        card.find(".add-service[data-type='addon']").click();
+                    }
+                });
+            });
+        }, 300); // ⏳ 等卡片與選單建立完成後填入
+    }
+
+    return {
+        saveLastBooking,
+        restoreLastBooking
+    };
+})();
