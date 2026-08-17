f81ef894d74c9b51695213c8ae466216ad000aa9
yongshengchen <>
2026-08-17 15:06:23 +0800

    LIFF_ID: '2010787602-WceTV9tT',   GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxql3uOcZA-mGuAX3PK0tz3gnHGWAP2RGKwJ2XnQYGhCPUV1QUCof_cJF61NcteuCIO/exec'

--- MembershipSystem/config.js
+++ MembershipSystem/config.js
@@ -1,4 +1,4 @@
 window.MEMBERSHIP_CONFIG = Object.freeze({
-  LIFF_ID: 'YOUR_LIFF_ID',
-  GAS_WEB_APP_URL: 'YOUR_GAS_WEB_APP_URL'
+  LIFF_ID: '2010787602-WceTV9tT',
+  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxql3uOcZA-mGuAX3PK0tz3gnHGWAP2RGKwJ2XnQYGhCPUV1QUCof_cJF61NcteuCIO/exec'
 });
