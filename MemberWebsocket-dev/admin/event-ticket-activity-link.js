ca0b5440a3f8f6559013637ceea789968e75d6cd
yongshengchen0615 <yongshengchen.tw@gmail.com>
2026-09-16 21:44:27 +0800

    feat(admin): load birthday benefit settings extension

--- MemberWebsocket-dev/admin/event-ticket-activity-link.js
+++ MemberWebsocket-dev/admin/event-ticket-activity-link.js
@@ -2,8 +2,19 @@
   'use strict';
 
   // Compatibility entrypoint retained by admin/index.html. Load isolated admin extensions here.
-  const script = document.createElement('script');
-  script.src = './pointcard-redemption-limit.js?v=global-ticket-limit-20260916-2';
-  script.defer = true;
-  document.head.append(script);
+  const scripts = [
+    './pointcard-redemption-limit.js?v=global-ticket-limit-20260916-2',
+    './birthday-benefits.js?v=birthday-benefits-20260916-1',
+  ];
+  scripts.forEach((src) => {
+    const script = document.createElement('script');
+    script.src = src;
+    script.defer = true;
+    document.head.append(script);
+  });
+
+  const birthdayStyles = document.createElement('link');
+  birthdayStyles.rel = 'stylesheet';
+  birthdayStyles.href = './birthday-benefits.css?v=birthday-benefits-20260916-1';
+  document.head.append(birthdayStyles);
 })();
