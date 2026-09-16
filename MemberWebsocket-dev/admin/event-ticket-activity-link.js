72e38f2da091769b76c30b8afa387b2fbc812c66
yongshengchen0615 <yongshengchen.tw@gmail.com>
2026-09-16 12:48:25 +0800

    chore(admin): refresh global point ticket settings loader

--- MemberWebsocket-dev/admin/event-ticket-activity-link.js
+++ MemberWebsocket-dev/admin/event-ticket-activity-link.js
@@ -3,7 +3,7 @@
 
   // Compatibility entrypoint retained by admin/index.html. Load isolated admin extensions here.
   const script = document.createElement('script');
-  script.src = './pointcard-redemption-limit.js?v=20260916-1';
+  script.src = './pointcard-redemption-limit.js?v=global-ticket-limit-20260916-2';
   script.defer = true;
   document.head.append(script);
 })();
