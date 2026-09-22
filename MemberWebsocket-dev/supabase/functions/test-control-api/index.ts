cc38ab937208a299d02175fd14af256a1495c211
yongshengchen0615 <yongshengchen.tw@gmail.com>
2026-09-22 15:17:25 +0800

    Raise bounded E2E record request limit

--- MemberWebsocket-dev/supabase/functions/test-control-api/index.ts
+++ MemberWebsocket-dev/supabase/functions/test-control-api/index.ts
@@ -9,7 +9,7 @@ type CaseResult = {
   actual: unknown;
 };
 
-const MAX_REQUEST_BYTES = 20_000;
+const MAX_REQUEST_BYTES = 384_000;
 const UUID_RE = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 class ApiError extends Error {
