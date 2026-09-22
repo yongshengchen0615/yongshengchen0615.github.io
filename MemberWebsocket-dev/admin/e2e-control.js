2c200c7168c4d01fe91eeb87ecd5da5d01765efb
yongshengchen0615 <yongshengchen.tw@gmail.com>
2026-09-22 14:55:52 +0800

    Bump fixed admin E2E controller version

--- MemberWebsocket-dev/admin/e2e-control.js
+++ MemberWebsocket-dev/admin/e2e-control.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const VERSION = '2026-09-22.5';
+  const VERSION = '2026-09-22.6';
   const TEST_SESSION_STORAGE_KEY = 'member-test-session-v1';
   const MAX_PAIRED_PARTICIPANTS = 10;
   const PAIRED_SURFACES = Object.freeze([
